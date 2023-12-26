43626a7cdaf3a2db03d35b12c8e30950c487aca5
Danshu <153497307+Dan-kin@users.noreply.github.com>
2023-12-26 19:03:59 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '通过我们的优势资源，协助更多对法国艺术深造、就业、互动等感兴趣的个人和机构更高效的实现目标🎉',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NouveauDÉPART', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '打通中法艺术之路，欢迎关注互动', // 作者简介
